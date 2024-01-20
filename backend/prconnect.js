97547664ac9ae10a14c1f414678d0346809c3aef
Katrina Lyman <lymankatrina@gmail.com>
2024-01-19 18:07:07 -0700

    Get things running

---
+++ backend/prconnect.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://student:2024BYUwork@cluster0.lnrb9fv.mongodb.net/test?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
