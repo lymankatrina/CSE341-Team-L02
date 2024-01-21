4e5fe1205637aedf71d1cf7b30526a9fe9bce08d
Katrina Lyman <lymankatrina@gmail.com>
2024-01-20 22:34:12 -0700

    fixed error in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
+const app = express();
+const port = process.env.PORT || 8080;
+
+
 const professionalRoutes = require('./routes/professional');
 
-const port = process.env.PORT || 8080;
-const app = express();
+
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
   })
   .use('/professional', professionalRoutes);
   
